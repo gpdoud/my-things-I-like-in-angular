94b39de22dfda7290fc49f20e609106f1fb20b7c
Greg doud <gpdoud@gmail.com>
2017-08-17 13:58:57 -0400

    Added input fields to add to the array and redisplay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { Thingtodo } from './thingstodo';
 
@@ -9,9 +10,19 @@ import { Thingtodo } from './thingstodo';
 })
 export class AppComponent {
 
+	thingtodo: Thingtodo = new Thingtodo(0, '', 0, 0);
+
 	thingstodo: Thingtodo[] = [
 		new Thingtodo(1, "Spelunking", 3, 100),
 		new Thingtodo(2, "Sleeping", 30, 0),
 		new Thingtodo(3, "Go to a movie", 2, 10)
 	];
+
+	add(): void {
+		console.log(this.thingtodo);
+		let ttd = new Thingtodo(this.thingtodo.order, this.thingtodo.name,
+										this.thingtodo.monthly, this.thingtodo.cost);
+		this.thingstodo.push(ttd);
+		this.thingtodo = new Thingtodo(0, '', 0, 0);
+	}
 }
